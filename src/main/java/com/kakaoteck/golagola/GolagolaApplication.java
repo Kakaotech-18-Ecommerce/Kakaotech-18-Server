c5a2a00f3797f6438a7dda15ad51c5aa0d16fe82
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-08-01 17:39:43 +0900

    SCRUM-8 feat: spring initializr

---
+++ src/main/java/com/kakaoteck/golagola/GolagolaApplication.java
@@ -0,0 +1,13 @@
+package com.kakaoteck.golagola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolagolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolagolaApplication.class, args);
+	}
+
+}
