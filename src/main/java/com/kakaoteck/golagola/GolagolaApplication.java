8488bb39aae9ee13874b5d2594e662eedb71ae20
Kim Seung-yeop <a01055143647@gmail.com>
2024-08-05 14:15:00 +0900

    SCRUM-10 feat: Goodbye 추가

--- src/main/java/com/kakaoteck/golagola/GolagolaApplication.java
+++ src/main/java/com/kakaoteck/golagola/GolagolaApplication.java
@@ -9,6 +9,7 @@ public class GolagolaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GolagolaApplication.class, args);
 		System.out.println("hello, world!");
+		System.out.println("GoodBye");
 	}
 
 }
