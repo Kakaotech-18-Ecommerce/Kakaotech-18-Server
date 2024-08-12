8d481c3b3f0ccadf11df369ece6783fde7b32ef8
Kim Seung-yeop <a01055143647@gmail.com>
2024-08-12 18:29:16 +0900

    SCRUM-48 feat: TokenRepository

--- src/main/java/com/kakaoteck/golagola/security/token/TokenRepository.java
+++ src/main/java/com/kakaoteck/golagola/security/token/TokenRepository.java
@@ -1,4 +1,19 @@
 package com.kakaoteck.golagola.security.token;
 
-public interface TokenRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    @Query(value = """
+            select t from Token t inner join User u on t.user.userId = u.userId
+            where u.userId = :id and (t.expired = false or t.revoked = false)
+            """)
+    List<Token> findAllValidTokenByUser(@Param("id") Long id);
+
+    Optional<Token> findByToken(String token);
 }
