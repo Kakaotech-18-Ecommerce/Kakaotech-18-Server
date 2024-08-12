3980fc7165fa5a936d134d15dce52a3357961e2a
Kim Seung-yeop <a01055143647@gmail.com>
2024-08-12 14:29:35 +0900

    SCRUM-35 feat: Buyer 엔티티 구성 완료

---
+++ src/main/java/com/kakaoteck/golagola/domain/buyer/entity/Buyer.java
@@ -0,0 +1,76 @@
+package com.kakaoteck.golagola.domain.buyer.entity;
+
+import com.kakaoteck.golagola.domain.buyer.entity.enums.Gender;
+import com.kakaoteck.golagola.domain.buyer.entity.enums.Role;
+import com.kakaoteck.golagola.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Buyer extends BaseEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long BuyerId;
+
+    private String nickname;
+    private String realName;
+    private Gender gender;
+    private String email;
+    private String address;
+    private String phoneNum;
+    private Role role;
+    private LocalDate registerDate;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/java/com/kakaoteck/golagola/domain/buyer/entity/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.kakaoteck.golagola.domain.buyer.entity.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
