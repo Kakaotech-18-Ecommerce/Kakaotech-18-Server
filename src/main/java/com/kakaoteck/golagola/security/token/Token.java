44cba9d44cc5c46a4279aa5eca7b154eeb1b04fc
Kim Seung-yeop <a01055143647@gmail.com>
2024-08-12 18:30:48 +0900

    SCRUM-48 feat: Token 클래스 작성

--- src/main/java/com/kakaoteck/golagola/security/token/Token.java
+++ src/main/java/com/kakaoteck/golagola/security/token/Token.java
@@ -1,4 +1,44 @@
 package com.kakaoteck.golagola.security.token;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.kakaoteck.golagola.domain.buyer.entity.Buyer;
+import com.kakaoteck.golagola.domain.seller.entity.Seller;
+import com.kakaoteck.golagola.security.token.enums.TokenType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token")
 public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType = TokenType.BEARER;
+
+    private boolean revoked;
+
+    private boolean expired;
+
+    @JsonIgnore
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id")
+    private Buyer buyer;
+
+    @JsonIgnore
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
 }
